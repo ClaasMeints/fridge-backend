e2c7c8d603ec9a24baf6756d368987003e0669e9
Claas Meints <claas.meints@outlook.com>
2022-09-30 22:55:12 +0200

    Fixed Bug (this not known in 2. Level Chained Higher Order Function)

--- src/device_content/device_content.service.ts
+++ src/device_content/device_content.service.ts
@@ -8,55 +8,6 @@ import { device_content } from './device_content.entity';
 
 @Injectable()
 export class device_content_service {
-  constructor(
-    @Inject('SEQUELIZE')
-    private sequelize: Sequelize,
-    private product_service: product_service,
-    @Inject('DEVICE_CONTENT_REPOSITORY')
-    private device_content_repository: typeof device_content,
-  ) {
-    this.device_content_repository = sequelize.getRepository(device_content);
-  }
-
-  private async aggregate(device_contents: device_content[]) {
-    return device_contents.map((device_content) => {
-      return this.product_service
-        .get_product_data(device_content.product.ean)
-        .then((product_data) => {
-          console.log('product_data', product_data);
-          return {
-            name:
-              product_data?.name ||
-              device_content.product.product_class.class_name,
-            image:
-              product_data?.image ||
-              device_content.product.product_class.class_image,
-            quantity_str: product_data?.quantity,
-            quantity: 1,
-            unit_symbol: device_content.product.product_class.unit.unit_symbol,
-            expiry_date: device_content.filled_in,
-          } as device_content_dto;
-        });
-    });
-  }
-
-  private async filter_duplicates(device_content_dtos: device_content_dto[]) {
-    return device_content_dtos
-      .map((device_content_dto) => {
-        return {
-          ...device_content_dto,
-          quantity: device_content_dtos.filter(
-            (content) => content.name === device_content_dto.name,
-          ).length,
-        };
-      })
-      .filter(
-        (device_content_dto, index, self) =>
-          index ===
-          self.findIndex((content) => content.name === device_content_dto.name),
-      );
-  }
-
   private join_products = {
     model: this.sequelize.models.product,
     as: 'product',
@@ -77,8 +28,52 @@ export class device_content_service {
     ],
   };
 
+  constructor(
+    @Inject('SEQUELIZE')
+    private sequelize: Sequelize,
+    private product_service: product_service,
+    @Inject('DEVICE_CONTENT_REPOSITORY')
+    private _device_content_repository: typeof device_content,
+  ) {
+    this._device_content_repository = sequelize.getRepository(device_content);
+  }
+
+  private aggregate = async (device_content) => {
+    const product_data = await this.product_service.get_product_data(
+      device_content.product.ean,
+    );
+    console.log('product_data', product_data);
+    return {
+      name:
+        product_data?.name || device_content.product.product_class.class_name,
+      image:
+        product_data?.image || device_content.product.product_class.class_image,
+      quantity_str: product_data?.quantity,
+      quantity: 1,
+      unit_symbol: device_content.product.product_class.unit.unit_symbol,
+      expiry_date: device_content.filled_in,
+    } as device_content_dto;
+  };
+
+  private filter_duplicates = (device_content_dtos: device_content_dto[]) => {
+    return device_content_dtos
+      .map((device_content_dto) => {
+        return {
+          ...device_content_dto,
+          quantity: device_content_dtos.filter(
+            (content) => content.name === device_content_dto.name,
+          ).length,
+        };
+      })
+      .filter(
+        (device_content_dto, index, self) =>
+          index ===
+          self.findIndex((content) => content.name === device_content_dto.name),
+      );
+  };
+
   async findAll(login: string): Promise<device_content_dto[]> {
-    return this.device_content_repository
+    return this._device_content_repository
       .findAll({
         raw: true,
         nest: true,
@@ -101,14 +96,16 @@ export class device_content_service {
           this.join_products,
         ],
       })
-      .then(this.aggregate)
+      .then((device_contents: device_content[]) => {
+        return device_contents.map(this.aggregate);
+      })
       .then((device_content_dtos) => {
         return Promise.all(device_content_dtos).then(this.filter_duplicates);
       });
   }
 
   async findAllFromDevice(device_id: number): Promise<device_content_dto[]> {
-    return this.device_content_repository
+    return this._device_content_repository
       .findAll({
         raw: true,
         nest: true,
@@ -119,7 +116,9 @@ export class device_content_service {
         },
         include: [this.join_products],
       })
-      .then(this.aggregate)
+      .then((device_contents: device_content[]) => {
+        return device_contents.map(this.aggregate);
+      })
       .then((device_content_dtos) => {
         return Promise.all(device_content_dtos).then(this.filter_duplicates);
       });
@@ -129,7 +128,7 @@ export class device_content_service {
     device_id: number,
     device_content_dto: product_dto,
   ): Promise<device_content> {
-    return this.device_content_repository.create({
+    return this._device_content_repository.create({
       device_id: device_id,
       product_id: (await this.product_service.create(device_content_dto))
         .product.product_id,
@@ -140,7 +139,7 @@ export class device_content_service {
     device_id: number,
     device_content_dto: product_dto,
   ): Promise<device_content> {
-    return this.device_content_repository
+    return this._device_content_repository
       .findOne({
         where: { device_id: device_id },
         include: [
